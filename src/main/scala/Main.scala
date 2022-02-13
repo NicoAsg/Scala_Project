f0e5f695d45c91377e11282983326ff6eddec9e1
NicoAsg <nicolas.alol@yahoo.fr>
2022-02-13 20:18:04 +0000

    FileReader finished

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,14 @@
+import fileReader.service.CSV
+import fileReader.model.AirportUtil
+import fileReader.model.CountryUtil
+import fileReader.model.RunwayUtil
+
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello World!")
+    
+    
+    
+
   }
 }
